5e81086d097041f9c76927d201e2dc6bc7c3cf96
mikel <55508637+apesteguia@users.noreply.github.com>
2022-11-23 18:06:19 +0100

    segundo

--- main.js
+++ main.js
@@ -1,38 +1,65 @@
 const canvas = document.querySelector("canvas");
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = 600;
+canvas.height = 600;
 const ctx = canvas.getContext("2d");
+
+ctx.rect(0, 0, canvas.width, canvas.height);
 
 const Snake = (direction, x, y) => ({
   x,
   y,
-  w: 15,
-  h: 15,
+  w: 20,
+  h: 20,
+  mov: 20,
   color: "green",
   draw(d) {
     ctx.fillStyle = this.color;
     if (d === "top") {
-      this.y = this.y - 15;
+      this.y = this.y - this.mov;
     } else if (d === "down") {
-      this.y = this.y + 15;
+      this.y = this.y + this.mov;
     } else if (d === "left") {
-      this.x = this.x - 15;
+      this.x = this.x - this.mov;
     } else if (d === "right") {
-      this.x = this.x + 15;
+      this.x = this.x + this.mov;
     }
     ctx.fillRect(this.x, this.y, this.w, this.h);
+  },
+  posicion() {
+    let pos = [];
+    pos.push(this.x);
+    pos.push(this.y);
+    return pos;
   },
 });
 
 const Comida = () => ({
-  w: 15,
-  h: 15,
+  w: 20,
+  h: 20,
+  x: 0,
+  y: 0,
   color: "red",
+  new() {
+    ctx.fillStyle = this.color;
+    this.x = Math.floor(Math.random() * 590);
+    while (this.x % 20 !== 0) {
+      this.x = Math.floor(Math.random() * 590);
+    }
+    this.y = Math.floor(Math.random() * 590);
+    while (this.y % 20 !== 0) {
+      this.y = Math.floor(Math.random() * 590);
+    }
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  },
   draw() {
     ctx.fillStyle = this.color;
-    let x = Math.floor(Math.random() * 800);
-    let y = Math.floor(Math.random() * 800);
-    ctx.fillRect(x, y, this.w, this.h);
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  },
+  posicion() {
+    let pos = [];
+    pos.push(this.x);
+    pos.push(this.y);
+    return pos;
   },
 });
 
@@ -55,35 +82,83 @@ const eventos = () => {
   return x;
 };
 
-const snake = Snake("", 400, 400);
-
 function checkKey(e) {
   const key = e.keyCode;
   const LEFT = 37;
   const UP = 38;
   const RIGHT = 39;
   const DOWN = 40;
-  switch (true) {
-    case key == LEFT:
-      snake.draw("left");
-      break;
-    case key == UP:
-      snake.draw("top");
-      break;
-    case key == RIGHT:
-      snake.draw("right");
-      break;
-    case key == DOWN:
-      snake.draw("down");
-      break;
+  let posS = [];
+  let posC = [];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < snake.length; i++) {
+    switch (true) {
+      case key == LEFT:
+        posS = [];
+        posC = [];
+        snake[i].draw("left");
+        posS = snake[i].posicion();
+        posC = comida.posicion();
+        comida.draw();
+        if (posS[0] == posC[0] && posS[1] == posC[1]) {
+          comida.new();
+          comida.draw();
+          snake[i].draw();
+          let s = Snake(snake[i].x - 20, snake[i].y - 20);
+          snake.push(s);
+        }
+        break;
+      case key == UP:
+        posS = [];
+        posC = [];
+        snake[i].draw("top");
+        posS = snake[i].posicion();
+        posC = comida.posicion();
+        comida.draw();
+        if (posS[0] == posC[0] && posS[1] == posC[1]) {
+          comida.new();
+          comida.draw();
+          snake[i].draw();
+        }
+        break;
+      case key == RIGHT:
+        posS = [];
+        posC = [];
+        snake[i].draw("right");
+        posS = snake[i].posicion();
+        posC = comida.posicion();
+        comida.draw();
+        if (posS[0] == posC[0] && posS[1] == posC[1]) {
+          comida.new();
+          comida.draw();
+          snake[i].draw();
+        }
+        break;
+      case key == DOWN:
+        posS = [];
+        posC = [];
+        snake[i].draw("down");
+        posS = snake[i].posicion();
+        posC = comida.posicion();
+        comida.draw();
+        if (posS[0] == posC[0] && posS[1] == posC[1]) {
+          comida.new();
+          comida.draw();
+          snake[i].draw();
+        }
+        break;
+    }
+  }
+  rellenar();
+}
+
+function rellenar() {
+  for (let i = 0; i < snake.length; i++) {
+    snake[i] = document.addEventListener("keydown", checkKey);
   }
 }
-
 const comida = Comida();
-comida.draw();
-snake.draw();
-
-function draw() {
-  document.addEventListener("keydown", checkKey);
-  ctx.clearRect(0,0, canvas.width, canvas.height);
-}
+comida.new();
+let snake = [];
+snake[0] = Snake("", 0, 0);
+snake[0].draw();
---
+++ snake.js
@@ -0,0 +1,95 @@
+const canvas = document.querySelector("canvas");
+canvas.width = 600;
+canvas.height = 600;
+const ctx = canvas.getContext("2d");
+
+ctx.rect(0, 0, canvas.width, canvas.height);
+
+const Snake = (direction, x, y) => ({
+  x,
+  y,
+  w: 20,
+  h: 20,
+  mov: 20,
+  color: "green",
+  draw(d) {
+    ctx.fillStyle = this.color;
+    if (d === "top") {
+      this.y = this.y - this.mov;
+    } else if (d === "down") {
+      this.y = this.y + this.mov;
+    } else if (d === "left") {
+      this.x = this.x - this.mov;
+    } else if (d === "right") {
+      this.x = this.x + this.mov;
+    }
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  },
+  posicion() {
+    let pos = [];
+    pos.push(this.x);
+    pos.push(this.y);
+    return pos;
+  },
+});
+
+const Comida = () => ({
+  w: 20,
+  h: 20,
+  x: 0,
+  y: 0,
+  color: "red",
+  new() {
+    ctx.fillStyle = this.color;
+    this.x = Math.floor(Math.random() * 590);
+    while (this.x % 20 !== 0) {
+      this.x = Math.floor(Math.random() * 590);
+    }
+    this.y = Math.floor(Math.random() * 590);
+    while (this.y % 20 !== 0) {
+      this.y = Math.floor(Math.random() * 590);
+    }
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  },
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  },
+  posicion() {
+    let pos = [];
+    pos.push(this.x);
+    pos.push(this.y);
+    return pos;
+  },
+});
+
+function checkKey(e) {
+  const key = e.keyCode;
+  const LEFT = 37;
+  const UP = 38;
+  const RIGHT = 39;
+  const DOWN = 40;
+  switch (true) {
+    case key == LEFT:
+      snake[0].draw("left");
+      break;
+    case key == UP:
+      snake[0].draw("top");
+      break;
+    case key == RIGHT:
+      snake[0].draw("right");
+      break;
+    case key == DOWN:
+      snake[0].draw("down");
+      break;
+  }
+}
+
+const comida = Comida();
+comida.new();
+
+let snake = [];
+snake[0] = Snake("", 0, 0);
+snake[0].draw();
+
+snake[0] = document.addEventListener("keydown", checkKey);
